9424b99f7133f48ff3f7ad0ff4c2a427ce8a73a3
ERIC <webberic92@yahoo.com>
2020-04-29 22:08:38 -0400

    backend

---
+++ Backend_Boilerplate/src/main/java/com/webb/Backend_Boilerplate/BackendBoilerplateApplication.java
@@ -0,0 +1,13 @@
+package com.webb.Backend_Boilerplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendBoilerplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendBoilerplateApplication.class, args);
+	}
+
+}
---
+++ Backend_Boilerplate/src/test/java/com/webb/Backend_Boilerplate/BackendBoilerplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webb.Backend_Boilerplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendBoilerplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
